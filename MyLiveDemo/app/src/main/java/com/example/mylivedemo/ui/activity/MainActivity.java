60a9ab984286fcc89b2a5167ea35f3b33191e2db
olderJiao <1286041884@qq.com>
2019-12-26 20:30:41 +0800

    olderJiao

--- MyLiveDemo/app/src/main/java/com/example/mylivedemo/ui/activity/MainActivity.java
+++ MyLiveDemo/app/src/main/java/com/example/mylivedemo/ui/activity/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.mylivedemo.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.example.mylivedemo.R;
@@ -15,6 +20,14 @@ import com.tencent.rtmp.ui.TXCloudVideoView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private RadioGroup group;
+    private RadioButton home_rb;
+    private RadioButton mine_rb;
+
+    private FrameLayout frameLayout;
+    private ImageView live_btn;
+
+    private FragmentManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,13 +41,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     private void initView() {
 
+        //获取控件
+        group = findViewById(R.id.main_bottom);
+        home_rb = findViewById(R.id.bottom_home);
+        mine_rb = findViewById(R.id.bottom_mine);
+        //设置点击事件
+        findViewById(R.id.start_live_btn).setOnClickListener(this);
+
+        manager = getSupportFragmentManager();
+
+
+
+        group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.bottom_home:
+
+                        break;
+                    case R.id.bottom_mine:
+
+                        break;
+                }
+            }
+        });
 
     }
 
     @Override
     public void onClick(View v) {
 
+        switch (v.getId()) {
+            case R.id.start_live_btn:
+
+                break;
+        }
+
     }
+
 
 
 }
