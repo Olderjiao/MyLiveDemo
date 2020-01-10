89611364ad1aa958edc04dc343935d72a055f8d9
olderJiao <1286041884@qq.com>
2020-01-10 11:43:08 +0800

    2020.01.10更新

---
+++ MyLiveDemo/app/src/main/java/com/example/mylivedemo/ui/widget/BottomBehavior.java
@@ -0,0 +1,105 @@
+package com.example.mylivedemo.ui.widget;
+
+import android.animation.Animator;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewPropertyAnimator;
+import android.view.animation.Interpolator;
+
+import androidx.annotation.NonNull;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.view.ViewCompat;
+import androidx.interpolator.view.animation.FastOutSlowInInterpolator;
+
+public class BottomBehavior extends CoordinatorLayout.Behavior<View> {
+    private static final Interpolator INTERPOLATOR = new FastOutSlowInInterpolator();
+    private float viewY;
+    //    控件距离coordinatorLayout底部距离
+    private boolean isAnimate;//动画是否在进行
+
+    public BottomBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onStartNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull View child, @NonNull View directTargetChild, @NonNull View target, int axes, int type) {
+        if (child.getVisibility() == View.VISIBLE && viewY == 0) {
+            //获取控件距离父布局（coordinatorLayout）底部距离
+            viewY = coordinatorLayout.getHeight() - child.getY();
+        }
+        // ViewCompat是一个兼容类,在android5.0之前的API为了实现新的效果
+        // 避免出错使用ViewCompat.xxxx方法可以解决出现低版本错误的问题
+        return (axes & ViewCompat.SCROLL_AXIS_VERTICAL) != 0;//判断是否竖直滚动
+    }
+
+    @Override
+    public void onNestedPreScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull View child, @NonNull View target, int dx, int dy, @NonNull int[] consumed, int type) {
+        super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type);
+        //dy大于0是向上滚动 小于0是向下滚动，判断的时候尽量不要判断是否大于等于或者小于等于0，否则可能会影响点击事件
+        if (type == ViewCompat.TYPE_TOUCH) {
+            if (dy > 0 && !isAnimate && child.getVisibility() == View.VISIBLE) {
+                hide(child);
+            } else if (dy < 0 && !isAnimate && child.getVisibility() == View.INVISIBLE) {
+                show(child);
+            }
+        }
+
+    }
+
+    private void show(final View view) {
+        ViewPropertyAnimator animator = view.animate().translationY(0).setInterpolator(INTERPOLATOR).setDuration(500);
+        animator.setListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {
+                view.setVisibility(View.VISIBLE);
+                isAnimate = true;
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                isAnimate = false;
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+                hide(view);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
+            }
+        });
+        animator.start();
+
+    }
+
+    private void hide(final View view) {
+        ViewPropertyAnimator animator = view.animate().translationY(viewY).setInterpolator(INTERPOLATOR).setDuration(500);
+        animator.setListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {
+                isAnimate = true;
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                view.setVisibility(View.INVISIBLE);
+                isAnimate = false;
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+                show(view);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
+            }
+        });
+        animator.start();
+
+    }
+}
